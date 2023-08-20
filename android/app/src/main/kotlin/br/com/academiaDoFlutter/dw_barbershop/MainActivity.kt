67fb26ad905d1d01c0a9a76729458c4eb623d07a
Guilhermessena <guilhermedvsena@gmail.com>
2023-08-20 15:05:58 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/academiaDoFlutter/dw_barbershop/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.academiaDoFlutter.dw_barbershop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/core/ui/barbershop_theme.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+
+sealed class BarbershopTheme {
+  static ThemeData themeData = ThemeData(
+    inputDecorationTheme: const InputDecorationTheme()
+  );
+}
